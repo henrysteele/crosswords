9369920efaad70314cfc33030ca969c20394d821
chadsteele <chad@chadsteele.com>
2024-06-09 05:40:31 +0400

    close, but no

--- src/CrosswordPuzzle.jsx
+++ src/CrosswordPuzzle.jsx
@@ -14,7 +14,75 @@ console.log({ keys })
 const [done, setDone] = createSignal(false)
 const [tip, showTip] = createSignal()
 
-function createLabelMatrix (matrix) {
+function getPeers (pos) {
+
+    pos = assessPos(pos)
+
+    const peers = []
+
+    if (!pos) return peers
+    if (matrix[pos.r][pos.c] == 1) return peers
+
+
+    if (pos.horizontalWord) {
+        // peers to the left
+        let r = pos.r
+        for (let c = pos.c; c >= 0; c--) {
+            if (matrix[r][c] == 1) break;
+            peers.push({ r, c })
+        }
+
+        // peers to the right
+        for (let c = pos.c; c < matrix.length; c++) {
+            if (matrix[r][c] == 1) break;
+            peers.push({ r, c })
+        }
+    }
+
+    if (pos.verticalWord) {
+        // peers above
+        let c = pos.c
+        for (let r = pos.r; r >= 0; r--) {
+            if (matrix[r][c] == 1) break;
+            peers.push({ r, c })
+        }
+
+        // peers below
+        for (let r = pos.r; r < matrix.length; r++) {
+            if (matrix[r][c] == 1) break;
+            peers.push({ r, c })
+        }
+    }
+
+    return [... new Set(peers.map(p => `r${p.r}c${p.c}`))].sort((a, b) => a.localeCompare(b))
+}
+
+function assessPos (pos) {
+    if (!matrix || !pos) return pos
+    if (pos.r == undefined || pos.c == undefined) return pos
+    const p = pos
+    const len = matrix.length
+    let { r, c } = pos
+
+    // is there a 1 to the left or right?
+    p.oneLeft = c ? matrix[r][c - 1] == 1 : true
+    p.oneRight = c < len - 1 ? matrix[r][c + 1] == 1 : true
+    p.horizontalWord = !p.oneLeft || !p.oneRight
+
+    // is there a 1 above or below?
+    p.oneAbove = r ? matrix[r - 1][c] == 1 : true
+    p.oneBelow = r < len - 1 ? matrix[r + 1][c] == 1 : true
+    p.verticalWord = !p.oneAbove || !p.oneBelow
+
+    // start of word?
+    p.startAcross = p.oneLeft && !p.oneRight  // is there a 1 to the left and a word to the right?
+    p.startDown = p.oneAbove && !p.oneBelow  // is there a 1 above and not below
+    p.startOfWord = p.startAcross || p.startDown
+
+    return p
+}
+
+function createLabels (matrix) {
     const len = matrix.length
     const labels = newMatrix(len)
     const transposedMatrix = transpose(cloneMatrix(matrix))
@@ -23,23 +91,14 @@ function createLabelMatrix (matrix) {
     for (let r = 0; r < len; r++) {
         for (let c = 0; c < len; c++) {
             if (matrix[r][c] == 1) continue // not a letter
-
-            // we're looking for a letter with a 1 to the left or above and another letter to the right or below
-            const left = c ? matrix[r][c - 1] == 1 : true
-            const right = c < len - 1 ? matrix[r][c + 1] == 1 : true
-            const above = r ? matrix[r - 1][c] == 1 : true
-            const below = r < len - 1 ? matrix[r + 1][c] == 1 : true
-            const across = left && !right
-            const down = above && !below
-
-            const startOfWord = across || down
-            if (startOfWord) {
+            const p = assessPos({ r, c })
+            if (p.startOfWord) {
                 labels[r][c] = count
-                if (across) {
+                if (p.startAcross) {
                     const word = matrix[r].slice(c).join("").split("1")[0]
                     words.across[count] = { word, hint: "" }
                 }
-                if (down) {
+                if (p.startDown) {
                     const word = transposedMatrix[c].slice(r).join("").split("1")[0]
                     words.down[count] = { word, hint: "" }
                 }
@@ -54,7 +113,7 @@ function createLabelMatrix (matrix) {
 export default function CrosswordPuzzle (props) {
     matrix = props.value
     usersMatrix = newMatrix(matrix.length, matrix.length, "1")
-    labelMatrix = createLabelMatrix(matrix)
+    labelMatrix = createLabels(matrix)
 
     return <>
         <Stack orientation="horizontal">
@@ -79,6 +138,7 @@ function Hints (props) {
         showTip(n)
         const element = document.getElementById(n)
         element?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        element.focus()
     }
 
     return <>
@@ -116,11 +176,13 @@ function Row (props) {
 
 
 function Cell (props) {
-    const { pos } = props
+    let { pos } = props
     const blank = [1, 2, 3, "1", "2", "3"].includes(props.value)
     const [value, setValue] = createSignal(props.value)
-    const [label, setLabel] = createSignal()
-    const [hint, setHint] = createSignal()
+    const [label, setLabel] = createSignal("")
+    const [hint, setHint] = createSignal(<></>)
+    const [style, setStyle] = createSignal({})
+    const [peers, setPeers] = createSignal([])
 
     onMount(() => {
         setLabel(labelMatrix[pos.r][pos.c])
@@ -137,6 +199,47 @@ function Cell (props) {
                 {down}
             </Show>
         </>)
+        pos = assessPos(pos)
+        setPeers(getPeers(pos))
+    })
+
+    function findPos (n) {
+        const len = labelMatrix.length
+        for (let r = 0; r < len; r++)
+            for (let c = 0; c < len; c++)
+                if (labelMatrix[r][c] == n) return { r, c }
+    }
+
+    function findLabel (pos) {
+        if (pos.startOfWord == undefined) pos = assessPos(pos)
+        const { r, c } = pos
+        const m = matrix
+
+        if (pos.horizontalWord) {
+            for (let c = pos.c; c >= 0; c--) {
+                if (matrix[r][c] == 1) break
+                const label = labelMatrix[r][c]
+                if (label) return label
+            }
+        }
+
+        if (pos.verticalWord) {
+            for (let r = pos.r; r >= 0; r--) {
+                if (matrix[r][c] == 1) break
+                const label = labelMatrix[r][c]
+                if (label) return label
+            }
+        }
+        return false
+    }
+
+    createEffect(() => {
+        if (!peers().length) return
+        if (!tip()) return
+        const { r, c } = findPos(tip())
+        const rc = `r${r}c${c}`
+        const style = peers().includes(rc) ? { opacity: 1 } : { opacity: .5 }
+        setStyle(style)
     })
 
     function onKeyDown (e) {
@@ -148,12 +251,23 @@ function Cell (props) {
         }
     }
 
+    function onClick () {
+        pos = (assessPos(pos))
+        setPeers(getPeers(pos))
+        const n = findLabel(pos)
+        showTip(n)
+    }
+
     return <>
 
-        <td class={blank ? "black" : "white"}>
+        <td>
             <Show when={!blank}>
                 <Show when={label()}><div class="label">{label()}</div></Show>
-                <input id={label() || ""} minLength={1} maxLength={1} value={value()} onKeyDown={onKeyDown} />
+                <input id={label() || ""} minLength={1} maxLength={1} value={value()}
+                    onKeyDown={onKeyDown}
+                    onClick={onClick}
+                    style={style()}
+                />
                 <Popper
                     open={label() == tip()}
                     anchorEl={document.getElementById(label())}
